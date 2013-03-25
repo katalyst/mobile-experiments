57072c05ebb57051a9758b72eb9b47e3fa3ffc26
Haydn Ewers <haydn@katalyst.com.au>
2013-03-25 13:42:39 +1030

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileNav::Application.config.secret_token = '8c6f1ac1156834a2527b809115702cf0e7bb5cfbd3b1ebf160fa617f0479bed8dd4816ee4f814fbb0d4ffbcb9cdf377c737e74946a1ad7f9c8ac9daf546fe8fb'
